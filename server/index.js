61e59734f8956a48c6424015fe84eca586a3d6ad
Sahil Imrose <arkhasan16@gmail.com>
2024-04-09 22:05:41 +0600

    half done

--- server/index.js
+++ server/index.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-const port = 3000;
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "sahilimrosez@gmail.com",
+    pass: process.env.GMAIL_PASS,
+  },
+});
+
+const port = 3000 || process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 const uri =
-  "mongodb+srv://sahilimrosezahin:1598753@cluster0.pvlfj2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  `mongodb+srv://sahilimrosezahin:${process.env.MONGO_PASS}@cluster0.pvlfj2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -69,7 +83,8 @@ async function run() {
       res.send({ status: "updated" });
     });
     app.post("/api/verifyEmail", async (req, res) => {
-      
+      const id = req.body.id;
+      const email = req.body.email;
       function stringGen(len) {
         var text = "";
 
@@ -81,8 +96,14 @@ async function run() {
         return text;
       }
 
-      console.log(stringGen(64));
-      res.send(stringGen(64));
+      const info = await transporter.sendMail({
+        from: "Verify Email <contact@sahilimrose.com>", // sender address
+        to: email, // list of receivers
+        subject: "Verify Email", // Subject line
+        html: `<p>In order to verify your Dribbble account click <a href=${`http://localhost:5173/verify/${id}/token/${stringGen(
+          64
+        )}`}>here.</a></p>`, // html body
+      });
     });
   } finally {
     // await client.close();
